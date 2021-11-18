b68dc03733bd66bf321ff9c86be59845028c35a4
stevekim813 <steve@stevekim.co>
2021-11-18 12:04:13 -0800

    product description tabs + product metafields

--- assets/theme.js
+++ assets/theme.js
@@ -9685,3 +9685,37 @@ function removeImageLoadingAnimation(image) {
     imageWrapper.removeAttribute('data-image-loading-animation');
   }
 }
+
+// tab selector //
+
+// function to remove show-active class & add to clicked
+function setActiveContent(el) {
+  // select all .content-body
+  var contentBodies = document.getElementsByClassName('content-body');
+  // remove .show-active
+  for (var contentBody of contentBodies) {
+      contentBody.classList.remove('show-active');
+  }
+  // add .show-active (back) to clicked 
+  document.
+      getElementById(el.textContent.trim())
+      .classList.add('show-active');
+}
+
+function setActiveTab(el){
+  var tabs = document.getElementsByClassName('tab');
+  for (var tab of tabs) {
+      tab.classList.remove('tab-active');
+  }
+  el.classList.add('tab-active');
+}
+
+// select .tab class, and loop through
+var tabs = document.getElementsByClassName('tab');
+for (var tab of tabs) {
+  // add click listener to each tab
+  tab.addEventListener('click', function(e) {
+      setActiveContent(e.currentTarget);
+      setActiveTab(e.currentTarget);
+  })
+}
